5aaaf24d05ab5cfbcafa20d49b068dfa2b78ad2b
kfjt <k.fujita@jcom.home.ne.jp>
2019-10-16 06:44:17 +0900

    feat: add Geolocation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,8 +19,33 @@ function App() {
           Learn React
         </a>
       </header>
+      <Geolocation />
     </div>
   );
+}
+
+const Geolocation = () => {
+  const [coords, setCoords] = useState(({}))
+
+  const {geolocation} = navigator
+
+  useEffect(() => {
+    if (geolocation && geolocation.getCurrentPosition) {
+      geolocation.getCurrentPosition(({coords}) => {
+        console.log(coords)
+        const {accuracy, latitude, longitude} = coords
+        setCoords({accuracy, latitude, longitude})
+      })
+    }
+  }, [geolocation])
+  
+  return (
+    <div className="geolocation">
+      <div >{`accuracy:${coords.accuracy}`}</div>
+      <div >{`latitude:${coords.latitude}`}</div>
+      <div >{`longitude:${coords.longitude}`}</div>
+    </div>
+  )
 }
 
 export default App;
