a7c554c9288e206934dc24186155b86c1d613748
unknown <U031025@bxo.local>
2019-10-17 12:30:09 +0900

    feat: rm app header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Geolocation />
     </div>
-  );
+  )
 }
 
 const Geolocation = () => {
@@ -51,4 +36,4 @@ const Geolocation = () => {
   )
 }
 
-export default App;
+export default App
